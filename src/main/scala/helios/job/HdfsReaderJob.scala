887e1ba69d8b136cf3648755ac5529192037abec
ankang.chen <ankang.chen@transwarp.io>
2023-11-20 16:41:55 +0800

    first commit

--- src/main/scala/helios/job/HdfsReaderJob.scala
+++ src/main/scala/helios/job/HdfsReaderJob.scala
@@ -8,11 +8,19 @@ object HdfsReaderJob {
       .master("local")
       .appName(HdfsReaderJob.getClass.getName)
       .getOrCreate()
+    spark.read
+      .option("inferSchema", true)
+      .parquet("hdfs://172.26.0.129:8220/")
+  }
+
+  private def readFromHdfs(): Unit = {
+    val spark = SparkSession.builder()
+      .master("local")
+      .appName(HdfsReaderJob.getClass.getName)
+      .getOrCreate()
     val df = spark.read
       .option("inferSchema", true)
       .csv("hdfs://localhost:9000/spark/tmp/t1.csv")
     df.show()
-
-
   }
 }
--- src/main/scala/helios/job/HdfsWriteJob.scala
+++ src/main/scala/helios/job/HdfsWriteJob.scala
@@ -1,5 +1,8 @@
 package helios.job
 
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.security.UserGroupInformation
 import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructType}
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 
@@ -8,14 +11,21 @@ import scala.util.Random
 
 object HdfsWriteJob {
   def main(args: Array[String]): Unit = {
-    val principal = "hdfs/tw-node130@TDH"
-    val path = "hdfs://localhost:9000/spark/tmp/t1.csv"
+    val principal = "hdfs/tw-node131@TDH"
+    val dir = "/Users/chenankang/Desktop/helios/project/spark/spark-example/"
+    val resource = "src/main/resources/"
+    val keytab = s"$dir${resource}hdfs.keytab"
+    val krb5Conf = s"$dir${resource}krb5.conf"
+    //    val path = "hdfs://localhost:9000/spark/tmp/t1.csv"
+    val path = "hdfs://172.26.0.129:8220/myjfs/hdfs/t1.csv"
     val spark = SparkSession.builder()
       .master("local")
       .appName(HdfsWriteJob.getClass.getName)
+      .config("spark.authenticate", "true")
+      .config("hadoop.security.authentication", "kerberos")
       .getOrCreate()
 
-    val lines = 10000
+    val lines = 100
     val rows: Array[Row] = new Array[Row](lines)
     for (i <- Array.range(0, lines)) {
       val name = s"name_$i"
@@ -31,10 +41,25 @@ object HdfsWriteJob {
       .add(StructField("email", StringType, true))
       .add(StructField("location", StringType, true))
     val df = spark.createDataFrame(rows.toList.asJava, schema)
-    spark.conf()
+
+    // 设置Kerberos认证的Spark配置
+    //    spark.sparkContext.hadoopConfiguration.set("spark.authenticate", "true")
+    //    spark.sparkContext.hadoopConfiguration.set("spark.yarn.principal", principal)
+    //    spark.sparkContext.hadoopConfiguration.set("spark.yarn.keytab", keytab)
+    //    spark.sparkContext.hadoopConfiguration.set("spark.yarn.krb5.conf", krb5Conf)
+    val conf: Configuration = new Configuration
+    conf.set("hadoop.security.authentication", "kerberos")
+    //        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");//        conf.set("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem");
+    System.setProperty("java.security.krb5.conf", "/etc/krb5.conf")
+    UserGroupInformation.setConfiguration(conf)
+    UserGroupInformation.loginUserFromKeytab(principal, keytab)
+    spark.sparkContext.hadoopConfiguration.set("hadoop.security.authentication", "kerberos")
+    spark.conf.set("hadoop.security.authentication", "kerberos")
     df.write
       .mode(SaveMode.Overwrite)
       .option("header", "true")
+      .option("hadoop.security.authentication", "kerberos")
+      .option("fs.hdfs.impl", "org.apache.hadoop.hdfs.DistributedFileSystem")
       .csv(path)
 
   }
